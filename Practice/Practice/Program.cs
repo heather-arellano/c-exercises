59c5398ed3362a02bbf8ca042f5297a7b27ea5df
HArellano <harellano@americanpayroll.org>
2019-11-05 11:02:05 -0600

    Lsit with names complete

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            //Adding names
+            names.Add("Dan");
+            names.Add("Ryan");
+            names.Add("Ann");
+            names.Add("Sarah");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
